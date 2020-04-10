c4d652e1c0e077b0b62bf21399e13a6ae3e84205
abhijit1998 <abhijittudu98@gmail.com>
2020-04-10 12:35:19 +0530

    Create aicp-potter-clean-full.sh

---
+++ build_scripts/aicp-potter-clean-full.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#CLEAN BUILD DIRECTORY
+rm -rf ~/builds/aicp-15.0
+
+#MAKING BUILD DIRECTORY
+mkdir ~/builds/aicp-15.0
+cd ~/builds/aicp-15.0/
+
+#ADDING AICP SOURCE
+repo init -u https://github.com/AICP/platform_manifest.git -b q10.0
+
+#ADDING MOTO G5 PLUS (POTTER) DEVICE TREE, KERNEL, VENDOR AND OTHER SOURCE FOR BUILDING AICP-15.0
+mkdir .repo/local_manifests
+FILE=".repo/local_manifests/roomservice.xml"
+/bin/cat <<EOM >$FILE
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest>
+  <project path="device/motorola/potter" name="abhijit1998/aicp_device_potter" remote="github" revision="q10.0" />
+  <project path="vendor/motorola" name="PixelExperience-Devices/vendor_motorola" remote="github" revision="ten" />
+  <project path="kernel/motorola/potter" name="GZR-Kernels/Optimus_Drunk_Potter" remote="github" revision="10.0" />
+  <project path="packages/resources/MotoActions" name="ArrowOS-Devices/packages_resources_MotoActions" remote="github" revision="arrow-10.0" />
+  <project path="system/qcom" name="LineageOS/android_system_qcom" remote="github" revision="lineage-17.0" />
+</manifest>
+EOM
+
+#REPO SYNC / DOWNLOAD SOURCE FILES
+repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
+
+#USE CCACHE (OPTIONAL)
+export USE_CCACHE=1
+export CCACHE_EXEC=$(command -v ccache)
+
+#BUILD ROM FOR MOTO G5 PLUS (POTTER)
+. build/envsetup.sh
+lunch aicp_potter-userdebug
+mka bacon -j$(nproc --all)
