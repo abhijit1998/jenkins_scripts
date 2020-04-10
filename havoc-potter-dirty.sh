30e88f5e34d098ee514e4a1abc45fe33a85798c7
abhijit1998 <abhijittudu98@gmail.com>
2020-04-10 12:20:47 +0530

    Create havoc-potter-dirty.sh

---
+++ havoc-potter-dirty.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd ~/builds/havoc-10.0/
+
+#USE CCACHE (OPTIONAL)
+export USE_CCACHE=1
+export CCACHE_EXEC=$(command -v ccache)
+
+#BUILD ROM FOR MOTO G5 PLUS (POTTER)
+. build/envsetup.sh
+lunch havoc_potter-userdebug
+mka bacon -j$(nproc --all)
