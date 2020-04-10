db2edcfb918c1fe8270d9612c6309fe707f09f14
abhijit1998 <abhijittudu98@gmail.com>
2020-04-10 12:19:33 +0530

    cleanup

--- build_havoc-10_potter.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/builds/havoc-10.0/
-. build/envsetup.sh
-lunch havoc_potter-userdebug
-mka bacon -j$(nproc --all)
--- havoc-10_start_tg.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SENDING TELEGRAM NOTIFICATIONS
-cd /home/abhijittudu98/python_scripts/
-rm -rf url.py
-echo "build_url='${BUILD_URL}'" >> url.py
-python3 havoc-10.0_start.py
